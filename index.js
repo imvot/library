3a402e89261c0c7e0eeb4e890d009c92ea146944
imvot <110433315+imvot@users.noreply.github.com>
2024-01-10 22:10:33 -0500

    Add book class
    Added basic book creation js code
    Removed all books except template
    Added style to hide template

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-function Book(title, author, nbPages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.nbPages = nbPages;
-    this.read = isRead;
-    this.info = function() {
-        readMsg = this.read ? "Already Read" : "Not Read Yet";
-        return `${this.title} ${this.author}, ${this.nbPages} pages, ${readMsg}`;
-    }
+function getTemplate() {
+    const original = document.querySelector("#template");
+    const template = original.cloneNode(true);
+    template.id = "";
+    return template;
 }
 
-const book = new Book("The Hobbit", "Tolkien", 295, true);
-console.log(book.info())
+const main = document.querySelector("main");
+const library = []
+
+class Book {
+    constructor(title, author, nbPages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.nbPages = nbPages;
+        this.isRead = isRead;
+        this.id = library.length;
+        library[this.id] = this;
+        this.create()
+    }
+
+    create() {
+        const newBook = getTemplate();
+        newBook.id = this.id;
+        main.appendChild(newBook);
+        this.update()
+    }
+
+    update() {
+        const book = document.getElementById(this.id);
+        const title = book.querySelector(".book-itemTitle");
+        title.innerText = this.title;
+        const author = book.querySelector(".book-itemAuthor");
+        author.innerText = this.author;
+        const nbPages = book.querySelector(".book-itemPages");
+        nbPages.innerText = `${this.nbPages} pages`;
+        const readStatus = book.querySelector(".book-itemStatus");
+        readStatus.innerText = this.isRead ? "Read" : "Not Read"
+    }
+}
